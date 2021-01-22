099f938685dad09eddeae3897a43b9033e95a27a
alex-pfeifer <apfeif.12@gmail.com>
2021-01-22 08:46:29 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character.js";
+import axios from "axios";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  font-size: 50px;
+  font-weight: bold;
+  text-decoration: underline;
+  color: white;
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [charData, setCharData] = useState([]);
+  useEffect(() => {
+    const apiData = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((res) => {
+          setCharData(res.data.results);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    apiData();
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledH1 className="Header">Rick and Morrrrrrrrty Characters!</StyledH1>
+      <Character charData={charData} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,57 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  color: white;
+  padding-left: 30%;
+  padding-right: 30%;
+  border: 5px;
+  border-style: inset;
+  border-color: white;
+`;
+
+const StyledImg = styled.img`
+  padding-top: 5%;
+`;
+
+const StyledSpan = styled.span`
+  color: red;
+`;
+
+const StyledH2 = styled.h2`
+  text-decoration: underline;
+`;
+
+export default function Character(props) {
+  return (
+    <div>
+      {props.charData.map((data) => (
+        <StyledDiv className="container">
+          <StyledImg src={data.image} alt="char img"></StyledImg>
+          <StyledH2>
+            <StyledSpan>Name: </StyledSpan>{data.name}
+          </StyledH2>
+          <h4>
+            <StyledSpan>Species: </StyledSpan>{data.species}
+          </h4>
+          <h4>
+            <StyledSpan>Gender: </StyledSpan>{data.gender}
+          </h4>
+          <h4>
+            <StyledSpan>Status: </StyledSpan>{data.status}
+          </h4>
+          <h4>
+            <StyledSpan>Episodes: </StyledSpan>{data.episode.length}
+          </h4>
+          <h3>
+            <StyledSpan>Origin: </StyledSpan>{data.origin.name}
+          </h3>
+          <h3>
+            <StyledSpan>LAST KNOWN LOCATION: </StyledSpan> <br></br>
+            {data.location.name}
+          </h3>
+        </StyledDiv>
+      ))}
+    </div>
+  );
+}
